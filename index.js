dedbce0dc5a0b6120c063ff07aa9eceae0086ad1
jgeorgilas <justin.georgilas@gmail.com>
2023-01-05 21:20:16 -0800

    updated JS

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+
+}
+let dogname = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogname.length; i++){
+	dogname[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = "orange"
+footer.style.borderStyle = 'solid'
